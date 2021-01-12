b4ba90498932ed4d3c656fe5d5e667fc16d29e74
ManBytesDog <ThomasJ.Marstiller@yahoo.com>
2021-01-12 09:45:07 -0500

    commit tree dae01d69da4f7c21c184765e954271e5c3083340

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+let drivers = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendDriver(name) {
+    drivers.push(name);
+};
+
+function destructivelyPrependDriver(name) {
+    drivers.unshift(name);
+};
+
+function destructivelyRemoveLastDriver() {
+    drivers.pop();
+};
+
+function destructivelyRemoveFirstDriver() {
+    drivers.shift();
+};
+
+function appendDriver(name) { 
+    const array1 = [...drivers, name]
+    return array1
+};
+
+function prependDriver(name) {
+    const array1 = [name, ...drivers]
+    return array1
+};
+
+function removeLastDriver() {
+   return drivers.slice(0, 2);
+ };
+
+ function removeFirstDriver() {
+     return drivers.slice(1)
+ }
